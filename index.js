c3c4a2a70a57ecac352a42e55cb95d1468a6aae8
GabrielOgliari <125907742+GabrielOgliari@users.noreply.github.com>
2025-03-13 23:33:43 +0000

    ajsutes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { createServer } = require('http');
 const express = require('express'); // responsavel get, post delete
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); // responsavel por carregar as variaveis de ambiente
+const bodyParser = require('body-parser'); // responsavel por pegar os dados do corpo da requisicao
 
 dotenv.config(); // carrega as variaveis de ambiente
-
 let app = express(); // criacao de servidor express
-
 const http = createServer(app); // criacao de servidor http
 
+//toda vez que o servidor receber uma requisicao, para json
+app.use(bodyParser.json()); // para pegar os dados do corpo da requisicao
 
 // call back é uma funcao que executa após um determinado evendo acontecer ( se tiver algum erro apresenta no console o erro)
 process.on('SIGINT', () => http.close((error => {
@@ -20,5 +21,10 @@ process.on('SIGINT', () => http.close((error => {
 app.get(
     '/',(_, res) => res.send('<h1>Resposta !!!!!!!</h1>')
 )
+// quando quer que seja pedido algo para o servidor e ele devolve algo 
+app.post('/', function(req, res) {
+    const nome = req.body.nome; // pegu o que está no body da requesição
+    res.send(`Olá ${nome}!`); // responde a requisição
+})
 
 http.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`)); // servidor ouvindo na porta 3000
